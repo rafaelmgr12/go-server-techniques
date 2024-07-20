c9a58d6b242616a52b0e531e13bb2492a37adf78
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-07-20 11:30:16 -0300

    Add exercise: create timer middleware

---
+++ api_timer_middleware/main.go
@@ -0,0 +1,115 @@
+package main
+
+/**
+Please add new middleware that will measure the time taken by our APIs to respond in
+milliseconds and print the results to logs.
+
+**/
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var welcomeMsg = "Welcome to the graceful server! 💃🏼\n"
+
+type gracefulServer struct {
+	httpServer *http.Server
+	listener   net.Listener
+}
+
+func withSimpleLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Default().Printf("Incoming traffic on route %s", r.URL.Path)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+/*
+ * Add middleware function here
+ * func ...(..)...{..}
+ */
+
+func (server *gracefulServer) preStart() {
+	currentHandler := server.httpServer.Handler
+	loggerHandler := withSimpleLogger(currentHandler)
+	// INSERT CODE HERE
+	// myNewTimeHandler := ???
+	server.httpServer.Handler = loggerHandler // EDIT THIS ONCE DONE
+}
+
+func (server *gracefulServer) start() error {
+	listener, err := net.Listen(
+		"tcp",
+		server.httpServer.Addr)
+	if err != nil {
+		return err
+	}
+
+	server.listener = listener
+	go server.httpServer.Serve(server.listener)
+	log.Default().Printf("Server now listening on %s", server.httpServer.Addr)
+	return nil
+}
+
+func (s *gracefulServer) shutdown() error {
+	if s.listener != nil {
+		err := s.listener.Close()
+		s.listener = nil
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Default().Println("Shutting down server")
+	return nil
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello student!\n")
+}
+
+func greetingHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, welcomeMsg)
+}
+
+func newServer(port string) *gracefulServer {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handle)
+	mux.HandleFunc("/greeting", greetingHandler)
+
+	httpServer := &http.Server{Addr: ":" + port, Handler: mux}
+	return &gracefulServer{httpServer: httpServer}
+}
+
+func main() {
+	var port string
+	flag.StringVar(&port, "port", "8080", "./course_server -port 8080")
+	flag.Parse()
+
+	done := make(chan bool, 1)
+	interrupts := make(chan os.Signal, 1)
+	signal.Notify(interrupts, syscall.SIGINT, syscall.SIGTERM)
+
+	server := newServer(port)
+	server.preStart()
+	err := server.start()
+	if err != nil {
+		log.Fatalf("Error starting server - %v\n", err)
+	}
+
+	go func() {
+		sig := <-interrupts
+		log.Default().Printf("Signal intercepted - %v\n", sig)
+		server.shutdown()
+		done <- true
+	}()
+
+	<-done
+}
